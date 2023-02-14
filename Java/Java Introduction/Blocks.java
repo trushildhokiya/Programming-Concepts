ecf92acc4da45097ff89596908cba9530b99c334
Trushil <95337251+trushildhokiya@users.noreply.github.com>
2023-02-14 19:48:31 +0530

    {java / java Introduction /}: added Blocks.java file

---
+++ Java/Java Introduction/Blocks.java
@@ -0,0 +1,15 @@
+public class Blocks {
+    public static void main(String[] args) {
+        // a lock in java is a container stated and ended by curly braces can contain multiple lines of code
+        { //start of the block
+            System.out.println("hello im in block A");
+            //end of the block
+        }
+
+        //expression is statement without ';'
+        //statement add a ; to an expression it becomes valid java statement
+        int x=10;
+
+        System.out.println(x+ " is in main block");
+    }
+}
